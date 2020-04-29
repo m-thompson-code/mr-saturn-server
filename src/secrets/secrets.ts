5d65109083b1377681a66a09fbca8d09f2b41007
Mark Thompson <mark.thompson@smpl.company>
2020-04-28 21:08:36 -0700

    Added notes in sercrets

--- src/secrets/secrets.ts
+++ src/secrets/secrets.ts
@@ -1,9 +1,9 @@
 export const firebaseCerts = {
-    email: '',
-    password: '',
+    email: '',// STUB, PLEASE UPDATE THESE VALUES
+    password: '',// STUB, PLEASE UPDATE THESE VALUES
 };
 
 export const twitchCerts = {
-    BOT_USERNAME: '',
-    OAUTH_TOKEN: '',
+    BOT_USERNAME: '',// STUB, PLEASE UPDATE THESE VALUES
+    OAUTH_TOKEN: '',// STUB, PLEASE UPDATE THESE VALUES
 }
