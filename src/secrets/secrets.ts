d034ca6d38e6df46ee8a9c91cb5a3ab5ce100f88
Mark Thompson <mark.thompson@smpl.company>
2020-04-28 21:20:04 -0700

    deleted secrets

--- src/secrets/secrets.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseCerts = {
-    email: '',// STUB, PLEASE UPDATE THESE VALUES
-    password: '',// STUB, PLEASE UPDATE THESE VALUES
-};
-
-export const twitchCerts = {
-    BOT_USERNAME: '',// STUB, PLEASE UPDATE THESE VALUES
-    OAUTH_TOKEN: '',// STUB, PLEASE UPDATE THESE VALUES
-}
